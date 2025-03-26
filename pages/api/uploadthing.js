a461a406f5614895cd5a6dd35893041a77c4b647
jeffdachis <jeff@informeddatasystems.com>
2025-03-26 19:05:20 -0400

    Update uploadthing.js

--- pages/api/uploadthing.js
+++ pages/api/uploadthing.js
@@ -1,14 +1,13 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   const body = req.body;
-  console.log("Dummy Upload body:", body);
 
-  return res.status(200).json({
-    url: "https://uploadthing.com/dummy/" + encodeURIComponent(body.name),
-    name: body.name,
-    type: body.type,
+  console.log("Dummy uploadthing request:", body);
+
+  res.status(200).json({
+    url: "https://uploadthing.com/dummy/" + encodeURIComponent(body.name)
   });
 }
